752c68a1dad455e61c27c042297213f021034795
scelikcapa <scelikcapa@outlook.com>
2022-01-18 06:01:18 -0500

    SECTION 2: 8.Lambda Expressions is over

--- CSharpAdvanceNET/OldLessonsAdvanceNET.cs
+++ CSharpAdvanceNET/OldLessonsAdvanceNET.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CSharpAdvanceNET.Delegates;
 using CSharpAdvanceNET.Generic;
+using CSharpAdvanceNET.LambdaExpressions;
 
 namespace CSharpAdvanceNET
 {
@@ -51,9 +52,49 @@ namespace CSharpAdvanceNET
             filterHandler += RemoveRedEyeFilter;
 
             processor.Process("photo.jpg", filterHandler);
+
+            // 8.LAMBDA EXPRESSIONS
+            // extra Square method needed without lambda expressions
+            // Func<int, int> square = Square;
+            // Console.WriteLine(Square(5));
+            Func<int, int> square = number2 => number2 * number2;
+            Console.WriteLine(square(5));
+
+            //static int Square(int number)
+            //{
+            //    return number * number;
+            //}
+
+            // with lambda expressions, dont have to extra method
+            // have to use delegates to work with lambda expressions
+            // args => expression
+            // nor args.... () => ....
+            // one args.... x => ....
+            // (x,y,z) => ....
+            // number => number*number;
+
+            const int factor = 5;
+            Func<int, int> multipler = n => n * factor;
+            var result = multipler(10);
+            Console.WriteLine(result);
+
+            // Common way to Use Lambda : where need Predicate Delegate
+            var books2 = new BookRepository().GetBooks();
+            // var cheapBooks = books.FindAll(IsCheaperThan10Dollars);
+            var cheapBooks = books2.FindAll(b => b.Price < 10);
+            foreach (var book2 in cheapBooks)
+            {
+                Console.WriteLine(book2.Title);
+            }
+
+            //static bool IsCheaperThan10Dollars(Book book)
+            //{
+            //    return book.Price < 10;
+            //}
+
             
         }
-        
+
         // using in 7.DELEGATES
         static void RemoveRedEyeFilter(Photo photo)
         {
--- CSharpAdvanceNET/Program.cs
+++ CSharpAdvanceNET/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 using CSharpAdvanceNET.Delegates;
 using CSharpAdvanceNET.Generic;
+using CSharpAdvanceNET.LambdaExpressions;
 using Microsoft.SqlServer.Server;
 
 namespace CSharpAdvanceNET
@@ -15,28 +16,12 @@ namespace CSharpAdvanceNET
     {
         static void Main(string[] args)
         {
-            // extra Square method needed without lambda expressions
-            // Console.WriteLine(Square(5));
-
-            // with lambda expressions, dont have to extra method
-            // have to use delegates to work with lambda expressions
-            // args => expression
-            // number => number*number;
+           
 
 
-            // Func<int, int> square = Square;
-            Func<int, int> square = number => number * number;
-            Console.WriteLine(square(5));
-
-
-            // VIDEO 5:00 
         }
-            
 
-        //static int Square(int number)
-        //{
-        //    return number * number;
-        //}
+        
         
     }
 }
