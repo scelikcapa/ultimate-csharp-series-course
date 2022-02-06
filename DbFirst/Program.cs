332a234b25a2286656c59da366181313c2278b51
scelikcapa <scelikcapa@outlook.com>
2022-02-06 09:47:50 -0500

    SECTION 3 is over

---
+++ DbFirst/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFirst
+{
+    public enum Level : byte
+    {
+        Beginner=1,
+        Intermediate=2,
+        Advanced=3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 17. IMPORTING STORED PROCEDURES 
+            var context = new PlutoDbContext();
+            var courses = context.GetCourses();
+
+            foreach (var c in courses)
+            {
+                Console.WriteLine(c.Title);
+            }
+
+
+            // 19. WORKING WITH ENUMS
+            var course = new Course();
+            // course.Level = CourseLevel.Beginner; // 1
+            course.Level = Level.Beginner;
+            
+        }
+    }
+}
---
+++ DbFirstExercisesVidzy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFirstExercisesVidzy
+{
+    public enum Classification:byte
+    {
+        Silver=1,
+        Gold=2,
+        Platinum=3
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var context = new VidzyContext();
+            //context.AddVideo("VideoTitle", DateTime.Now, "Action");
+            //context.SaveChanges();
+
+            //context.AddVideo("VideoTitle2", DateTime.Now, "Horror");
+            //context.AddVideo("VideoTitle3", DateTime.Now, "Family");
+            //context.AddVideo("VideoTitle4", DateTime.Now, "Romance");
+            context.AddVideo("VideoTitle5", DateTime.Now, "Thriller",Classification.Platinum);
+
+            context.SaveChanges();
+
+
+        }
+    }
+}
