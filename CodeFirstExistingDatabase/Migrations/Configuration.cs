20e3cd3c8306591e01e57475b0562f3ec83444e4
scelikcapa <scelikcapa@outlook.com>
2022-02-10 05:00:37 -0500

    EntityFramework Section 4 is over except Exercises

---
+++ CodeFirstExistingDatabase/Migrations/Configuration.cs
@@ -0,0 +1,39 @@
+using System.Collections.ObjectModel;
+
+namespace CodeFirstExistingDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodeFirstExistingDatabase.PlutoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CodeFirstExistingDatabase.PlutoContext context)
+        {
+            context.Authors.AddOrUpdate(a => a.Name,
+                new Author
+                {
+                    Name = "Author 1",
+                    Courses = new Collection<Course>()
+                    {
+                        new Course() {Name = "Course for Author 1", Description = "Description"}
+                    }
+                });
+
+
+
+
+
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
