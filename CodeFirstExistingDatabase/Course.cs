501dec8d3b918dc40502ee8ada12252bd3b24a03
scelikcapa <scelikcapa@outlook.com>
2022-02-18 08:02:35 -0500

    EntityFramework Section 5: 42. Organizing Fluent API Config is over

--- CodeFirstExistingDatabase/Course.cs
+++ CodeFirstExistingDatabase/Course.cs
@@ -7,6 +7,7 @@ namespace CodeFirstExistingDatabase
     using System.Data.Entity.Spatial;
 
     [Table("Courses")]
+    //[Table("Courses", Schema = "catalog")]
     public partial class Course
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -17,8 +18,36 @@ namespace CodeFirstExistingDatabase
 
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        //[Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.None)]
+        //public string ISBN { get; set; }
+        
+        
+        //[Key]
+        //[Column(Order=1)]
+        //public int OrderId { get; set; }
 
+        //[Key]
+        //[Column(Order=2)]
+        //public int OrderItemId { get; set; }
+
+        
+        //[Index(IsUnique = true)]
+        //public int AuthorId { get; set; }
+        
+        
+        //[Index("IX_AuthorStudentsCount",1)]
+        //public int AuthorId { get; set; }
+        
+        //[Index("IX_AuthorStudentsCount",2)]
+        //public int StudentsCount { get; set; }
+        
+        
+        //[Column("sName", TypeName = "varchar")]
+        //[MaxLength(255)]
+        public string Name { get; set; }
+        
+        //[Required]
         public string Description { get; set; }
         public int Level { get; set; }
 
