ab20ad4c069d3a13b4366a2f4767f09366b83947
scelikcapa <scelikcapa@outlook.com>
2022-03-06 08:06:07 -0500

    EntityFramework Section 5 is over except Exercises

---
+++ FluentAPI/EntityConfigurations/CourseConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAnnotations;
+
+namespace FluentAPI.EntityConfigurations
+{
+    public class CourseConfiguration : EntityTypeConfiguration<Course>
+    {
+        public CourseConfiguration()
+        {
+            ToTable("tbl_Course");
+
+            HasKey(c => c.Id);
+
+            // Properties
+            Property(c => c.Description)
+            .HasMaxLength(2000);
+
+            Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(255);
+            
+            // Relationships
+            HasRequired(c => c.Author)
+            .WithMany(a => a.Courses)
+            .HasForeignKey(c => c.AuthorId)
+            .WillCascadeOnDelete(false);
+
+            HasRequired(c => c.Cover)
+            .WithRequiredPrincipal(c => c.Course);
+
+            HasMany(c => c.Tags)
+                .WithMany(t => t.Courses)
+                .Map(m =>
+                {
+                    m.ToTable("CourseTags");
+                    m.MapLeftKey("CourseId");
+                    m.MapRightKey("TagId");
+                });
+        }
+    }
+}
